57c76c8f11370ead1a295008bb033dc307dcd7ce
KaramPark <com3892@gmail.com>
2025-07-30 10:22:33 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (  
+  <>
+    <div className="card">
+      <img src="/public/images.jpg" alt="images" />
+      <h2>람</h2>
+      <h2>programmer</h2>
+    </div>
+
+
+  </>
+  );
+}
+
+export default App
